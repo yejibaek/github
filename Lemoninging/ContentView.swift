e350af17197647e1980e642303f047b608395a25
yejibaek <qordpwl123@gmail.com>
2024-04-18 20:29:26 +0900

    First commit

--- Lemoninging/ContentView.swift
+++ Lemoninging/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-           Text("dd")
+           Text("Hellooooooooooo")
         }
         .padding()
     }
