f5b8f3530f6af10e2708a3dd5def01bbcd285024
yejibaek <yejibaek>
2024-04-18 20:14:12 +0900

    Initial Commit

---
+++ Lemoninging/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lemoninging
+//
+//  Created by yeji on 4/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+           Text("dd")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lemoninging/LemoningingApp.swift
@@ -0,0 +1,17 @@
+//
+//  LemoningingApp.swift
+//  Lemoninging
+//
+//  Created by yeji on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct LemoningingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
